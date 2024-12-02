56200a28e1a6fe53721ef86c6288e60fc4fc31c5
taylanhutt <160601352+taylanhutt@users.noreply.github.com>
2024-12-02 11:00:59 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,20 @@
+criaCartao(
+    'Programaçao',
+    'O que é JavaScript?',
+    'O JavaScript é uma linguagem de programaçao'
+)
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris.'
+)
+criaCartao(
+    'Programaçao',
+    'O que é uma funçao',
+    'uma funçao é um bloco de código que executa uma tarefa.'
+)
+criaCartao(
+    'lingua Inglesa',
+    'Como se diz oi em ingles?',
+    'oi em ingles se diz HI (rai)'
+)
